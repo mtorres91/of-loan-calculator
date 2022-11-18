63e080b55d5c7728b63d0913541544b6adbe2c76
mtorres91 <m.a.t.a.9125_@hotmail.com>
2022-11-18 15:12:38 -0600

    act

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   defaultLanguage: 'es',
-  appUrl: 'https://of-form.netlify.app/',
+  appUrl: 'https://form-prequal-home.netlify.app/',
   submitUrl: 'https://www.formbackend.com/f/8d5eaa5dff45959d',
   websiteUrl: 'https://grape-tan-3sbh.squarespace.com/',
   version: '1.0.2',
